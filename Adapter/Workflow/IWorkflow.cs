a0502e16458d1fb31315dd22990c47787555cea3
Wyy <wyy0709@qq.com>
2021-03-08 15:11:59 +0800

    Adapter

---
+++ Adapter/Workflow/IWorkflow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter.Workflow
+{
+    /// <summary>
+    /// 本代码文件不允许修改
+    /// </summary>
+    public interface IWorkflow
+    {
+        void Start();
+
+
+        void Suspend();
+
+
+        void Resume();
+
+
+        void Terminate(string reason);
+
+    }
+}
---
+++ Adapter/Workflow/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter.Workflow
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> ConsoleResponseAction = new Action<string>(ConsoleResponse);
+
+            while (true)
+            {
+                Console.Write("请输入要使用的Workflow版本（3 or 4）：");
+                string input = Console.ReadLine();
+                if (input == "0")
+                {
+                    break;
+                }
+
+                ConsoleResponseAction(input);
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        static void ConsoleResponse(string input)
+        {
+            if (input == "3" || input == "4")
+            {
+                switch (input)
+                {
+                    case "3":
+                        WF3Core wf3 = new WF3Core();
+                        wf3.StartWorkflow();
+                        wf3.TerminateWorkflow();
+                        break;
+                    case "4":
+                        IWorkflow wf4 = new WF4Core();
+                        wf4.Start();
+                        wf4.Terminate("haha~");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.Write("输入错误");
+            }
+        }
+    }
+}
